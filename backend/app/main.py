788811b7c32907e115fea13640794cd58aef249f
whims <whimsy73737@gmail.com>
2026-06-09 17:07:48 +0900

    fix: add vercel domain to cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,6 +11,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "https://studioflow-xi-gray.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
